d37f02409ea2df21a279a85696b6b92b16422104
mateusfg7 <mateusfelipefg77@gmail.com>
2020-09-18 18:09:53 -0300

    add source docs

---
+++ docs/source/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"GeoMath":{"doc":"","i":[[5,"main","GeoMath","",null,[[]]]],"p":[]},\
+"geo_math":{"doc":"","i":[[5,"main","geo_math","",null,[[]]]],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ docs/source/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["GeoMath"] = {"name":"","files":["main.rs"]};
+sourcesIndex["geo_math"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
