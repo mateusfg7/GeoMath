f2ed1b0b4699c8bb3e0d2802cd40a27feb2809e9
mateusfg7 <mateusfelipefg77@gmail.com>
2020-09-21 15:36:15 -0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use utils::format;
+use utils::interact;
+
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Base:");
+    let user_base = interact::read_input();
+
+    println!("Height:");
+    let user_height = interact::read_input();
+
+    let formated_base_str = format::trim_str(user_base);
+    let formated_height_str = format::trim_str(user_height);
+
+    let base = format::str2int(formated_base_str);
+    let height = format::str2int(formated_height_str);
+
+    let area = base * height;
+
+    println!("{}cm", area);
 }
