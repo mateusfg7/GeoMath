9d19913aff6424422dcaa76f40a4a57cf816a023
mateusfg7 <mateusfelipefg77@gmail.com>
2020-09-21 15:36:51 -0300

    docs

--- docs/search-index.js
+++ docs/search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = JSON.parse('{\
-"geo_math":{"doc":"","i":[[5,"main","geo_math","",null,[[]]]],"p":[]}\
+"geo_math":{"doc":"","i":[[5,"main","geo_math","",null,[[]]],[0,"utils","","",null,null],[0,"formatting","geo_math::utils","",null,null],[0,"format","geo_math::utils::formatting","",null,null],[5,"trim_str","geo_math::utils::formatting::format","",null,[[["string",3]],["string",3]]],[5,"str2int","","",null,[[["string",3]]]],[0,"user","geo_math::utils","",null,null],[0,"interact","geo_math::utils::user","",null,null],[5,"read_input","geo_math::utils::user::interact","",null,[[],["string",3]]]],"p":[]}\
 }');
 addSearchOptions(searchIndex);initSearch(searchIndex);
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["geo_math"] = {"name":"","files":["main.rs"]};
+sourcesIndex["geo_math"] = {"name":"","dirs":[{"name":"utils","files":["formatting.rs","mod.rs","user.rs"]}],"files":["main.rs"]};
 createSourceSidebar();
