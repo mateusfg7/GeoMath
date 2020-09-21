c8edf727c10ffb0f0a197d01307dbcd6ed8fa897
mateusfg7 <mateusfelipefg77@gmail.com>
2020-09-21 16:50:52 -0300

    refact: update utils module

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,6 @@
-pub use self::formatting::format;
-mod formatting;
+pub use self::format::str2int;
+pub use self::format::trim_str;
+pub mod format;
 
-pub use self::user::interact;
-mod user;
+pub use self::interact::read_input;
+pub mod interact;
