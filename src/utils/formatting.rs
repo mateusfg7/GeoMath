cfcf0d76e50affa1dfb7115b3394baad1d424ddf
mateusfg7 <mateusfelipefg77@gmail.com>
2020-09-21 16:50:12 -0300

    refact: clear format module

--- src/utils/formatting.rs
+++
@@ -1,9 +0,0 @@
-pub mod format {
-    pub fn trim_str(string: String) -> String {
-        string.trim().to_string()
-    }
-
-    pub fn str2int(string: String) -> i32 {
-        string.parse::<i32>().unwrap()
-    }
-}
