6fe4f4015dacd28e2e7eb4d82494741089e75c24
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-26 16:21:17 -0300

    refact make_trapezoid

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ use clap::ArgMatches;
 mod forms;
 use forms::circle::Circle;
 use forms::rhombus::Rhombus;
-use forms::trapezoid::Trapezoid;
 
 mod makers;
 use makers::square::make_square;
+use makers::trapezoid::make_trapezoid;
 use makers::triangle::{make_sides_triangle, make_simple_triangle};
 
 pub fn main() {
@@ -143,21 +143,5 @@ fn make_rhombus(l_diagonal: String, s_diagonal: String) -> Rhombus {
     return Rhombus {
         l_diagonal: formatted_larger_diagonal_int,
         s_diagonal: formatted_smaller_diagonal_int,
-    };
-}
-
-fn make_trapezoid(l_base: String, s_base: String, height: String) -> Trapezoid {
-    let formatted_larger_base_str = format::trim_str(l_base);
-    let formatted_smaller_base_str = format::trim_str(s_base);
-    let formatted_height_str = format::trim_str(height);
-
-    let formatted_larger_base_int = format::str2int(formatted_larger_base_str);
-    let formatted_smaller_base_int = format::str2int(formatted_smaller_base_str);
-    let formatted_height_int = format::str2int(formatted_height_str);
-
-    return Trapezoid {
-        l_base: formatted_larger_base_int,
-        s_base: formatted_smaller_base_int,
-        height: formatted_height_int,
     };
 }
